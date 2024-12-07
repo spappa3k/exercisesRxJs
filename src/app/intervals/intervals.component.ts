07c81d80a74775b1038bfffd9d146114787ba208
spappa3k <pappalardo.dev@gmail.com>
2024-12-07 10:31:45 +0100

    .

--- src/app/intervals/intervals.component.ts
+++ src/app/intervals/intervals.component.ts
@@ -7,15 +7,14 @@ import { interval } from 'rxjs';
   templateUrl: './intervals.component.html',
   styleUrl: './intervals.component.css'
 })
-export class IntervalsComponent implements OnInit{
+export class IntervalsComponent{
 
   public value:number=0;
 
 
-  ngOnInit(): void {
-    // il $ dopo il nome della variabile la imposta come Observable da poter sottoscrivere
-    const interval$= interval(1000);  // interval e'importato da rxjs 
 
+  countdownStarts(){
+    const interval$= interval(1000); 
     interval$.subscribe(x=>{    //sottoscriviamo
       this.value=x;
     })
