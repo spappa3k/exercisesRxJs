c4bafde1411ea7c62152760f24dd43c360641109
spappa3k <pappalardo.dev@gmail.com>
2024-12-07 11:01:42 +0100

    .

--- src/app/intervals/intervals.component.ts
+++ src/app/intervals/intervals.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
+import { interval, Subscription } from 'rxjs';
 
 
 @Component({
@@ -7,17 +7,36 @@ import { interval } from 'rxjs';
   templateUrl: './intervals.component.html',
   styleUrl: './intervals.component.css'
 })
-export class IntervalsComponent{
+export class IntervalsComponent {
 
-  public value:number=0;
+  value: number = 0;
+  counting: boolean = false
+  private intervalSubscribtion?: Subscription // dichiariamo una sottoscrizione
+  private beepAudio = new Audio('https://www.soundjay.com/buttons/sounds/beep-07a.mp3');
 
 
-  countdownStarts(){
+  countdownStarts() {
+    this.counting = true;
+
     // il $ dopo il nome della variabile la imposta come Observable da poter sottoscrivere
-    const interval$= interval(1000);  // interval e'importato da rxjs 
+    const interval$ = interval(1000);  // interval e'importato da rxjs 
 
-    interval$.subscribe(x=>{    //sottoscriviamo
-      this.value=x;
+    this.intervalSubscribtion = interval$.subscribe(x => {    //sottoscriviamo nella sottoscrizione
+      this.value = x;
+
+      if(this.value!=0){
+      this.playBeep();
+      }
     })
+  }
+
+  countdownStops() {
+    this.counting = false;
+    this.intervalSubscribtion?.unsubscribe();
+    this.value = 0;
+  }
+
+  playBeep(){
+   this.beepAudio.play();
   }
 }
