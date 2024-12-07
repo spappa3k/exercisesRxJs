6273eee1d4d83a16f9744871776bbd3e76b047e0
spappa3k <pappalardo.dev@gmail.com>
2024-12-07 10:13:39 +0100

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntervalsComponent } from './intervals/intervals.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntervalsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/intervals/intervals.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-intervals',
+  templateUrl: './intervals.component.html',
+  styleUrl: './intervals.component.css'
+})
+export class IntervalsComponent {
+
+}
