c2128b379586a2ed05ee7ee877a23993c3f8a27a
spappa3k <pappalardo.dev@gmail.com>
2024-12-08 10:58:19 +0100

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { IntervalsComponent } from './intervals/intervals.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", component : HomeComponent
+  },
+  {
+    path:"interval", component : IntervalsComponent
+  },
+  {
+    path:"timer", component :  IntervalsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IntervalsComponent } from './intervals/intervals.component';
+import { HomeComponent } from './home/home.component';
+import { TimerComponent } from './timer/timer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IntervalsComponent
+    IntervalsComponent,
+    HomeComponent,
+    TimerComponent
   ],
   imports: [
     BrowserModule,
