95f76a8762598bdd35a509b74852f1e8fe8e1761
spappa3k <pappalardo.dev@gmail.com>
2024-12-12 13:41:35 +0100

    .

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -9,15 +9,38 @@ import { Subscription, timer } from 'rxjs';
 export class TimerComponent implements OnInit{
 private timerSubscribtion?:Subscription
 value:number=0;
-
+private beepAudio = new Audio('https://www.soundjay.com/buttons/sounds/beep-07a.mp3');
+counting:boolean=false;
 
   ngOnInit(): void {
-    var timer$ = timer(3000, 1000);
+    this.go();
+  }
+
+  go(){
+    var timer$ = timer(3000, 1000);  // 3000ms time to wait before it starts and 1000ms the event created every second
     this.timerSubscribtion=timer$.subscribe((x)=>{
       this.value=x;
+
+      if(this.value!=0){
+        this.playBeep();
+        this.counting=true;
+        }else{
+          this.counting=false;
+        }
     })
   }
 
+  timerReset(){
+    this.timerSubscribtion?.unsubscribe();
+    this.value=0;
+    this.counting=false;
+    this.go();
+  }
+
+
+  playBeep(){
+    this.beepAudio.play();
+   }
 
 
   
