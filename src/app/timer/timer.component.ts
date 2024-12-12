6034c85d8ee8fba612b1403b4c153a18316048be
spappa3k <pappalardo.dev@gmail.com>
2024-12-12 13:26:01 +0100

    .

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription, timer } from 'rxjs';
 
 @Component({
   selector: 'app-timer',
   templateUrl: './timer.component.html',
   styleUrl: './timer.component.css'
 })
-export class TimerComponent {
+export class TimerComponent implements OnInit{
+private timerSubscribtion?:Subscription
+value:number=0;
 
+
+  ngOnInit(): void {
+    var timer$ = timer(3000, 1000);
+    this.timerSubscribtion=timer$.subscribe((x)=>{
+      this.value=x;
+    })
+  }
+
+
+
+  
 }
