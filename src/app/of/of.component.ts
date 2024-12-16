4ed62c5acb517cac2cda2e4a8c7814e79038e423
spappa3k <pappalardo.dev@gmail.com>
2024-12-16 06:48:22 +0100

    .

--- src/app/of/of.component.ts
+++ src/app/of/of.component.ts
@@ -14,8 +14,14 @@ export class OfComponent implements OnInit{
     let obs$= of(1,2,3,4,5);  // of crea un observable con qualsiasi tipo di dato
 
     obs$.pipe(
-      filter(x=>x%2===0)
+      filter(x=>x%2===0) //filtro i numeri pari in x
     )
+    .subscribe(
+      y=> // Qui usiamo "y" per rappresentare il valore filtrato, non uso x dinuovo per far capire
+        { this.value=y} // assegno il valore di y a value
+    )
+
+
   }
 
 
