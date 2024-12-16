c1581b46462b233c1a59ce927aa32e2f09db833b
spappa3k <pappalardo.dev@gmail.com>
2024-12-16 06:18:49 +0100

    .

--- src/app/of/of.component.ts
+++ src/app/of/of.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { of, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-of',
   templateUrl: './of.component.html',
   styleUrl: './of.component.css'
 })
-export class OfComponent {
+export class OfComponent implements OnInit{
 
+  obsOf?:Subscription
+
+  ngOnInit(): void {
+    let obs$= of(1,2,3,4,5);  // of crea un observable con qualsiasi tipo di dato
+  }
+
+
+ 
 }
