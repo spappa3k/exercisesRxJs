6a187900dfb3c5a9c9442861b8795302aec8c24b
spappa3k <pappalardo.dev@gmail.com>
2024-12-22 17:18:39 +0100

    .

--- src/app/beh-subject/beh-subject.component.ts
+++ src/app/beh-subject/beh-subject.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-beh-subject',
   templateUrl: './beh-subject.component.html',
   styleUrl: './beh-subject.component.css'
 })
-export class BehSubjectComponent {
 
+export class BehSubjectComponent implements OnInit{
+numero = new BehaviorSubject<number>(0);
+
+private intervalSubscribtion?:Subscription
+
+
+ngOnInit(): void {
+  console.log("numero:",this.numero);
+  this.goToInterval();
+}
+
+goToInterval(){
+  const interval$ = interval(1000);
+}
 }
