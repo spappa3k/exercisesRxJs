eaf605dae332c8333dbf2df0d11e74345980b516
spappa3k <pappalardo.dev@gmail.com>
2024-12-22 17:24:32 +0100

    .

--- src/app/beh-subject/beh-subject.component.ts
+++ src/app/beh-subject/beh-subject.component.ts
@@ -1,26 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { BehaviorSubject, interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-beh-subject',
   templateUrl: './beh-subject.component.html',
-  styleUrl: './beh-subject.component.css'
+  styleUrls: ['./beh-subject.component.css'], 
 })
+export class BehSubjectComponent implements OnInit, OnDestroy {
+  // BehaviorSubject per condividere il valore
+  numero = new BehaviorSubject<number>(0);
 
-export class BehSubjectComponent implements OnInit{
-numero = new BehaviorSubject<number>(0);
-value:number=0;
+  // Subscription per gestire l'interval
+  private intervalSubscription?: Subscription;
 
-private intervalSubscribtion?:Subscription
+  ngOnInit(): void {
+    // Iscriviti al BehaviorSubject per osservare i cambiamenti
+    this.numero.subscribe((valore) => {
+      console.log('Valore aggiornato dal BehaviorSubject:', valore);
+    });
 
+    // Avvia l'interval e aggiorna il BehaviorSubject
+    this.goToInterval();
+  }
 
-ngOnInit(): void {
-  console.log("numero:",this.numero);
-  this.goToInterval();
-}
+  goToInterval() {
+    const interval$ = interval(1000); // Ogni secondo
+    this.intervalSubscription = interval$.subscribe((x) => {
+      this.numero.next(x); // Aggiorna il valore nel BehaviorSubject
+    });
+  }
 
-goToInterval(){
-
-  const interval$ = interval(1000);
-}
+  ngOnDestroy(): void {
+    // Evita perdite di memoria annullando la subscription
+    this.intervalSubscription?.unsubscribe();
+  }
 }
