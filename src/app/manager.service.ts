05bf0a16c2cd1145ed8a7c2be184114df8656cfa
spappa3k <pappalardo.dev@gmail.com>
2024-12-29 18:15:52 +0100

    .

--- src/app/manager.service.ts
+++ src/app/manager.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ManagerService {
 
-  private inputSubject=new Observable<string>
+  // Crea un BehaviorSubject con un valore iniziale vuoto
+  private inputSubject=new BehaviorSubject<string>('');
 
- input$
+   // Observable pubblico a cui i componenti possono iscriversi
+  input$=this.inputSubject.asObservable();
+
   constructor() { }
+
+
+  updateObservable(value:string){
+    this.inputSubject.next(value) // Emette un nuovo valore
+  }
 }
