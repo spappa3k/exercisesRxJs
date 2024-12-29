1b5862707ccf13f48772787ec75a4587c05b0574
spappa3k <pappalardo.dev@gmail.com>
2024-12-29 18:11:29 +0100

    .

---
+++ src/app/manager.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ManagerService {
+
+  private inputSubject=new Observable<string>
+
+ input$
+  constructor() { }
+}
