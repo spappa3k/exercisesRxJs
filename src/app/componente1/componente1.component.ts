e1ee5219fddca1b6996c108a1e8f8e177843e9dd
spappa3k <pappalardo.dev@gmail.com>
2024-12-29 18:37:39 +0100

    .

--- src/app/componente1/componente1.component.ts
+++ src/app/componente1/componente1.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgModel } from '@angular/forms';
+import { ManagerService } from '../manager.service';
 
 @Component({
   selector: 'app-componente1',
@@ -9,7 +10,10 @@ import { NgModel } from '@angular/forms';
 export class Componente1Component {
 nomeUtente:string=''
 
+constructor(private ms:ManagerService){}
+
 immetti(event:Event){
 this.nomeUtente= (event.target as HTMLInputElement).value; // casta per prendere l input dall evento
+this.ms.updateObservable(this.nomeUtente); //mandiamo al Service
 }
 }
--- src/app/componente2/componente2.component.ts
+++ src/app/componente2/componente2.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ManagerService } from '../manager.service';
 
 @Component({
   selector: 'app-componente2',
   templateUrl: './componente2.component.html',
   styleUrl: './componente2.component.css'
 })
-export class Componente2Component {
-nomi?:string[]
+export class Componente2Component implements OnInit{
+inputValue:string=''
 
+constructor(private ms:ManagerService){}
+
+ngOnInit(): void {
+  
+this.ms.input$.subscribe((value)=>{
+this.inputValue=value;
+})
+}
 }
