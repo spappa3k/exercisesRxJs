af79de1ed2bfff3e2d2f32b86e07667d268c7657
spappa3k <pappalardo.dev@gmail.com>
2025-01-03 13:18:39 +0100

    .

--- src/app/prova2-componente2/prova2-componente2.component.ts
+++ src/app/prova2-componente2/prova2-componente2.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ManagerService } from '../manager.service';
 
 @Component({
   selector: 'app-prova2-componente2',
   templateUrl: './prova2-componente2.component.html',
   styleUrl: './prova2-componente2.component.css'
 })
-export class Prova2Componente2Component {
+export class Prova2Componente2Component implements OnInit{
 inputValueProva2:string=''
+
+constructor(private ms:ManagerService){}
+
+ngOnInit(): void {
+  this.ms.inputProva2$.subscribe((value)=>{
+this.inputValueProva2=value;
+  })
+}
+
 }
